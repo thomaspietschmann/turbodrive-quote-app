86b4a18349384fe7b6ad9d07c731085698f782c6
Thomas Pietschmann <thomas.pietschmann@gmail.com>
2022-07-25 12:29:19 +0200

    chapter 7

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -2,7 +2,7 @@ class QuotesController < ApplicationController
   before_action :set_quote, only: [:show, :edit, :update, :destroy]
 
   def index
-    @quotes = Quote.ordered
+    @quotes = current_company.quotes.ordered
   end
 
   def show
@@ -13,12 +13,12 @@ class QuotesController < ApplicationController
   end
 
   def create
-  @quote = Quote.new(quote_params)
+  @quote = current_company.quotes.build(quote_params)
 
   if @quote.save
     respond_to do |format|
       format.html { redirect_to quotes_path, notice: "Quote was successfully created." }
-      format.turbo_stream
+      format.turbo_stream { flash.now[:notice] = "Quote was successfully created." }
     end
   else
     render :new, status: :unprocessable_entity
@@ -30,7 +30,10 @@ end
 
   def update
     if @quote.update(quote_params)
-      redirect_to quotes_path, notice: "Quote was successfully updated."
+      respond_to do |format|
+        format.html { redirect_to quotes_path, notice: "Quote was successfully updated." }
+        format.turbo_stream { flash.now[:notice] = "Quote was successfully updated." }
+      end
     else
       render :edit, status: :unprocessable_entity
     end
@@ -41,14 +44,15 @@ end
 
     respond_to do |format|
       format.html { redirect_to quotes_path, notice: "Quote was successfully destroyed." }
-      format.turbo_stream
+      format.turbo_stream { flash.now[:notice] = "Quote was successfully destroyed." }
     end
   end
 
   private
 
   def set_quote
-    @quote = Quote.find(params[:id])
+    # @quote = Quote.find(params[:id])
+    @quote = current_company.quotes.find(params[:id])
   end
 
   def quote_params
--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,5 +1,8 @@
 class Quote < ApplicationRecord
+  belongs_to :company
+
   validates :name, presence: true
+
 
   scope :ordered, -> { order(id: :desc) }
 
@@ -13,5 +16,5 @@ class Quote < ApplicationRecord
   # after_update_commit -> { broadcast_replace_later_to 'quotes' }
   # after_destroy_commit -> { broadcast_remove_to 'quotes' }
 
-  broadcasts_to ->(quote) { "quotes" }, inserts_by: :prepend
+  broadcasts_to ->(quote) { [quote.company, "quotes"] }, inserts_by: :prepend
 end
