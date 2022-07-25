44328b6fc5eb3da528aa0d86676503e4d73faf11
Thomas Pietschmann <thomas.pietschmann@gmail.com>
2022-07-25 10:38:06 +0200

    chapter 5

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -2,4 +2,16 @@ class Quote < ApplicationRecord
   validates :name, presence: true
 
   scope :ordered, -> { order(id: :desc) }
+
+  # after_create_commit lambda {
+  #                       broadcast_prepend_later_to 'quotes'
+  #                       # partial: 'quotes/quote' # default is to_partial_path
+  #                       # locals: { quote: self } # default is model_name.element.to_sym => self
+  #                       # , target: 'quotes' # default target is model_name.plural
+  #                     }
+
+  # after_update_commit -> { broadcast_replace_later_to 'quotes' }
+  # after_destroy_commit -> { broadcast_remove_to 'quotes' }
+
+  broadcasts_to ->(quote) { "quotes" }, inserts_by: :prepend
 end
